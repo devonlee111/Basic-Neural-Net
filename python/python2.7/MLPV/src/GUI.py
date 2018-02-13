acbe40d069a914cad1367749b375cb31761ff011
devonlee111 <devonlee111@gmail.com>
2018-02-12 20:58:53 -0500

    Linked Slider Value and Entry Boxes
    Made it so that the values from the various sliders now display in the entry boxes when you move them.

--- python/python2.7/MLPV/src/GUI.py
+++ python/python2.7/MLPV/src/GUI.py
@@ -1,6 +1,7 @@
 import Tkinter as tk
+import threading
 
-class Gui:
+class Gui(threading.Thread):
 
 	lr = .01
 	learningType = 0
@@ -9,55 +10,97 @@ class Gui:
 
 	def __init__(self):
 		self.args = []
-		root = tk.Tk()
-		root.title("Neural Net Visualization")
-		root.geometry("%dx%d+0+0" % (root.winfo_screenwidth(), root.winfo_screenheight()))
 
-		frame = tk.Frame(root)
-		frame.pack()
-		lrSlider = tk.Scale(root, label = "Learning Rate", from_ = 0, to = 1, orient = tk.HORIZONTAL, length = 200, resolution = 0.001)
-		lrSlider.pack()
+		# Create the root window.
+		self.root = tk.Tk()
+		self.root.title("Neural Net Visualization")
+		self.root.geometry("%dx%d+0+0" % (self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
 
-		epochSlider = tk.Scale(root, label = "Training Epochs", from_ = 1000, to = 1000000, orient = tk.HORIZONTAL, length = 200, resolution = 1000)
-		epochSlider.pack()
+		# Create the frame for the canvas.
+		netFrame = tk.Frame(self.root, highlightbackground="black", highlightcolor="black", highlightthickness=1)
+		netFrame.pack(side = tk.RIGHT, fill = tk.BOTH, expand = 1)
 
-		errorSlider = tk.Scale(root, label = "Desired Error", from_ = 0, to = .3, orient = tk.HORIZONTAL, length = 200, resolution = 0.01)
-		errorSlider.pack()
+		# Create the canvas for the net representation.
+		net = tk.Canvas(netFrame)
+		net.configure(background='pink')
+		net.pack(fill = tk.BOTH, expand = 1)
 
-		activationFunction = tk.StringVar(root)
-		activationFunction.set("Tanh")
-		activationMenu = tk.OptionMenu(root, activationFunction, "Tanh", "Sigmoid", "ReLU", "LReLU")
-		activationMenu.pack()
+		# Create the frame for the settings.
+		settingsFrame = tk.Frame(self.root)
+		settingsFrame.pack(side = tk.LEFT, fill = tk.Y)
 
-		trainingType = tk.StringVar(root)
-		trainingType.set("Batch")
-		trainingMenu = tk.OptionMenu(root, trainingType, "Batch", "Stochastic")
-		trainingMenu.pack()
+		# Create a slider to select learning rate.
+		self.lrSlider = tk.Scale(settingsFrame, label = "Learning Rate", from_ = 0, to = 1, orient = tk.HORIZONTAL, resolution = 0.001, command = self.setLearningRate)
+		self.lrSlider.grid(row = 0, column = 0, sticky = tk.W)
 
+		# Create an entry box for custom learning rate.
+		self.lrInput = tk.Entry(settingsFrame)
+		self.lrInput.grid(row = 1, column = 0, sticky = tk.W, pady = 10)
 
-		root.mainloop()
+		# Create a slider to select number of epochs.
+		self.epochSlider = tk.Scale(settingsFrame, label = "Training Epochs", from_ = 1000, to = 1000000, orient = tk.HORIZONTAL, length = 200, resolution = 1000, command = self.setEpochs)
+		self.epochSlider.grid(row = 2, column = 0, sticky = tk.W)
 
-	def getLearningRate():
-		return lr
+		# Create an entry box for a custom epochs.
+		self.epochInput = tk.Entry(settingsFrame)
+		self.epochInput.grid(row = 3, column = 0, sticky = tk.W, pady = 10)
 
-	def setLearningRate(lr):
+		# Create an slider to select the error to stop at.
+		self.errorSlider = tk.Scale(settingsFrame, label = "Desired Error", from_ = 0, to = .3, orient = tk.HORIZONTAL, length = 200, resolution = 0.01, command = self.setError)
+		self.errorSlider.grid(row = 4, column = 0, sticky = tk.W)
+
+		# Create an entry box for a custom error.
+		self.errorInput = tk.Entry(settingsFrame)
+		self.errorInput.grid(row = 5, column = 0, sticky = tk.W, pady = 10)
+
+		# Create a dropdown menu to select activation function.
+		self.activationFunction = tk.StringVar(self.root)
+		self.activationFunction.set("Tanh")
+		self.activationMenu = tk.OptionMenu(settingsFrame, self.activationFunction, "Tanh", "Sigmoid", "ReLU", "LReLU")
+		self.activationMenu.grid(row = 6, column = 0, sticky = tk.W)
+
+		# Create a drowdown menu to select training type.
+		self.trainingType = tk.StringVar(self.root)
+		self.trainingType.set("Batch")
+		self.trainingMenu = tk.OptionMenu(settingsFrame, self.trainingType, "Batch", "Stochastic")
+		self.trainingMenu.grid(row = 7, column = 0, sticky = tk.W)	
+
+		# Start the window's mainloop.
+		self.root.mainloop()
+
+	def getLearningRate(self):
+		return self.lr
+
+	def setLearningRate(self, lr):
+		self.lrInput.delete(0, tk.END)
+		self.lrInput.insert(0, lr)
 		self.lr = lr
 
-	def getActivationFunction():
-		return activationFunction
+	def getEpochs():
+		return self.epochs
 
-	def setActivationFunction(activationFunction):
+	def setEpochs(self, maxEpochs):
+		self.epochInput.delete(0, tk.END)
+		self.epochInput.insert(0, maxEpochs)
+		self.maxEpochs = maxEpochs
+	
+	def getError(self):
+		return self.error
+
+	def setError(self, error):
+		self.errorInput.delete(0, tk.END)
+		self.errorInput.insert(0, error)
+		self.error = error
+
+	def getActivationFunction(self):
+		return self.activationFunction
+
+	def setActivationFunction(self, activationFunction):
 		self.activationFunction = activationFunction
 
-	def getLearningType():
-		return learningType
+	def getLearningType(self):
+		return self.learningType
 
-	def setLearningType(learningType):
+	def setLearningType(self, learningType):
 		self.learningType = learningType
-
-	def getMaxEpochs():
-		return maxEpochs
-
-	def setMaxEpochs(maxEpochs):
-		self.maxEpochs = maxEpochs
-
+		
--- python/python2.7/MLPV/src/Main.py
+++ python/python2.7/MLPV/src/Main.py
@@ -1,20 +1,14 @@
+import threading
 import MLPV
 import GUI
-import subprocess
 
 class Main:
-	net = MLPV.Net()
-	gui = GUI.Gui()
+	#net = MLPV.Net()
+	#gui = GUI.Gui()
 
 	def __init__(self):
 		self.data = []
-
-	def main():
-		net.initNet()
-
-	def run():
-		while(True):
-			if gui.getRestart == True:
-				net.initNet();
-
-Main()
+		self.net = MLPV.Net()
+		self.gui = GUI.Gui()
+		
+main = Main()
