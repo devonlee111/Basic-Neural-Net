cde9eac06c9dc034a7d326d8d3c88da6263da804
devonlee111 <devonlee111@gmail.com>
2018-02-15 15:14:22 -0500

    Added File For Neural Net
    Added a file with the basic structure of the neural net.

---
+++ java/src/MLP.java
@@ -0,0 +1,21 @@
+public class MLP {
+	public MLP() {
+	
+	}
+
+	public static void forwardPass() {
+
+	}
+
+	public static void backwardPass() {
+
+	}
+
+	public static void train() {
+
+	}
+
+	public static void main(String args) {
+
+	}
+}
