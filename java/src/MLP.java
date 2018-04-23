5d154b9198727713ba9ffce3fb77c8f7a4071a7a
devonlee111 <devonlee111@gmail.com>
2018-04-23 15:15:50 -0400

    Java Program Skeleton
    Added necessary global variables and blank functions for the java version of the program.

--- java/src/MLP.java
+++ java/src/MLP.java
@@ -1,13 +1,64 @@
+import java.util.HashMap;
+
 public class MLP {
-	public MLP() {
+
+	private int activationFunction = 0;
+	private int learningType;
+	private Matrix x;
+	private Matrix y;
+	private HashMap labels = new HashMap();
+	private int layers = 0;
+	private int[] shape;
+	private double lr = 0.001;
+	private double desiredAccuracy = .05;
+	private double momentum = .01;
+	private Matrix prevDelta;
+	private Matrix currentError;
+	private Matrix currentOutput;
+	private int totalEpochs = 0;
+	private int trainingData = 0;
+	private int batchSize = 100;
+	private Matrix weights;
+	private Matrix neurons;
+	private Matrix biases;
+
+	public MLP(String[] args) {
 	
 	}
 
-	public static void forwardPass() {
+	private void initTrainingData(String trainingFile, int size) {
+		
+	}
+
+	private void yOneHot(int numLabels, int[] y) {
 
 	}
 
-	public static void backwardPass() {
+	private void parseArgs(String[] args) {
+
+	}
+
+	private void initWeights() {
+
+	}
+
+	private void initNeurons() {
+
+	}
+
+	private void initBiases() {
+
+	}
+
+	private void initPersistent() {
+
+	}
+
+	private void forwardPass() {
+
+	}
+
+	private void backwardPass() {
 
 	}
 
@@ -15,7 +66,8 @@ public class MLP {
 
 	}
 
-	public static void main(String args) {
-
+	public static void main(String[] args) {
+		MLP net = new MLP(args);
+		net.train();
 	}
 }
